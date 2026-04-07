71f48808b24bb87c97652add697fdab0530c4619
abhaysahu1008@gmail.com <abhaysahugit@gmail.com>
2026-04-07 11:54:50 +0530

    Another issue fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import { HashRouter } from "react-router-dom";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./Pages/Dashboard";
 import Body from "./Components/Body";
 import Transaction from "./Pages/Transaction";
@@ -7,34 +6,16 @@ import Analytics from "./Pages/Analytics";
 import Landing from "./Pages/Landing";
 
 function App() {
-  const appRouter = createBrowserRouter([
-    {
-      path: "/",
-      element: <Landing />,
-    },
-    {
-      path: "/app",
-      element: <Body />,
-      children: [
-        {
-          path: "dashboard",
-          element: <Dashboard />,
-        },
-        {
-          path: "transactions",
-          element: <Transaction />,
-        },
-        {
-          path: "analytics",
-          element: <Analytics />,
-        },
-      ],
-    },
-  ]);
-
   return (
     <HashRouter>
-      <RouterProvider router={appRouter} />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/app" element={<Body />}>
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="transactions" element={<Transaction />} />
+          <Route path="analytics" element={<Analytics />} />
+        </Route>
+      </Routes>
     </HashRouter>
   );
 }
