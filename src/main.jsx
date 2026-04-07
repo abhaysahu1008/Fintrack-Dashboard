65aca7c7314474ebc49addc1e20867f56cc5cb18
abhaysahu1008@gmail.com <abhaysahugit@gmail.com>
2026-04-07 11:47:14 +0530

    HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 // import store from "./utils/appStore";
@@ -9,7 +10,9 @@ import store from "./utils/appStore";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <HashRouter>
+        <App />
+      </HashRouter>
     </Provider>
   </StrictMode>,
 );
